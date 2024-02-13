db36399d31787a15198dae1b3517d761fba58dbb
Prathamesh Jadhav <prathameshj0407@gmail.com>
2024-02-13 23:57:13 +0530

    feat: Clerk Authentication and drizle-orm connection to postgres databse

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+import type { Config } from "drizzle-kit";
+dotenv.config({ path: ".env" });
+
+export default {
+  driver: "pg",
+  schema: "./src/lib/db/schema.ts",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string,
+  },
+} satisfies Config;
